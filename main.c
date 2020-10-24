d4a4e719548afd533cc4cf0f8fc7417142edd2bd
SamuelLang13 <langsamuel10@gmail.com>
2020-10-24 09:40:57 +0200

    Algoritmus na free integer number not ok

--- main.c
+++ main.c
@@ -5,6 +5,9 @@ int main(void)
 {
     int number;
     int in;
+    int powNumber;
+    int restNumber;
+    int freeIntegerNumber;
     printf("Poradi:\n");
     while((in=scanf("%d",&number))!=EOF)
     {
@@ -14,6 +17,28 @@ int main(void)
             printf("Nespravny vstup.\n");
             return 0;
         }
+
+        for (int i = 1; i<=number; i++)
+        {
+            powNumber=i*i;
+            restNumber=number%i;
+            //printf("%d\n",restNumber);
+            if(powNumber*restNumber!=number)
+            {
+                freeIntegerNumber=1;
+            }
+            else
+            {
+                freeIntegerNumber=0;
+            }
+            
+        }
+        if(freeIntegerNumber==1)
+        {
+            printf("= %d\n",number);
+        }
+        
+
     }
     
 
