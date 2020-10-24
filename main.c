134d1014078cf3320cd2ccd6eb43256679674fda
SamuelLang13 <langsamuel10@gmail.com>
2020-10-24 12:51:11 +0200

    Algoritmus pre spoznanie cisla OK

--- main.c
+++ main.c
@@ -5,9 +5,13 @@ int main(void)
 {
     int number;
     int in;
-    int powNumber;
-    int restNumber;
-    int freeIntegerNumber;
+    int tmp;
+    //int i;
+    //int prev;
+    //int powNumber;
+    //int sqrtNumber;
+    //int restNumber;
+    //int freeIntegerNumber=0;
     printf("Poradi:\n");
     while((in=scanf("%d",&number))!=EOF)
     {
@@ -18,26 +22,33 @@ int main(void)
             return 0;
         }
 
-        for (int i = 1; i<=number; i++)
+        /*for (int i = 1; i <= number; i++)
         {
-            powNumber=i*i;
-            restNumber=number%i;
-            //printf("%d\n",restNumber);
-            if(powNumber*restNumber!=number)
-            {
-                freeIntegerNumber=1;
-            }
-            else
-            {
-                freeIntegerNumber=0;
-            }
             
-        }
-        if(freeIntegerNumber==1)
-        {
-            printf("= %d\n",number);
-        }
+        }*/
         
+
+        if(number%2==0)
+        {
+            number=number/2;
+        }
+        if(number%2==0)
+        {
+            printf("Neni to bezstvorcove cislo\n");        
+        }
+        for (int j = 3; j <= sqrt(number); j=j+2)
+        {
+            if(number%j==0)
+            {
+                number=number/j;
+
+                if(number%j==0)
+                {
+                    printf("Neni to bezstvorcove cislo\n");
+                }
+
+            }
+        }
 
     }
     
